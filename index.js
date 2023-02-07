0b8ec0d433ffae6f2ee45b04d8c14d789d9f4a17
Hemel Bourhan <hemelbourhan@gmailgit config --global user.email Bourhan>
2023-02-07 18:50:56 +0600

    add descrption of functions

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 //problem-1
 function mindGame(sampleInput){
+    /**
+     * mindGame function is used for simple mathmathics function . if input value is 5 . then at first is multiply by 3 then add 10 then divide by 2 at last substract by 5 .
+     */
     let output ;
     //error validation
     if( typeof sampleInput === 'number'){
@@ -18,6 +21,9 @@ console.log(finalOutput);
 //problem-2
 
 function evenOdd(inputString){
+    /**
+     * evenOdd function is check the input string lenght . if lenght is even return even otherwise it return odd .
+     */
     //error validation
    if(typeof inputString ===  'string'){
             if(inputString.length % 2 == 0){
@@ -39,6 +45,9 @@ function evenOdd(inputString){
 //problem-3
 
 function isLGSeven(number){
+    /**
+     * isLGSeven function is used for finding differenc from input . if input is less then 7 , then input multiply by 2 , if is grater then 7 , return the value after substract from 7 . 
+     */
 
     //error validation
     if(typeof number === 'number' ){
@@ -65,6 +74,9 @@ function isLGSeven(number){
 //problem-4
 
 function findingBadData(array) {
+    /**
+     * findingBadData function is used for verify the inputs data is bad or good. if inputs data is less then 0 , we define as bad data otherwise is a good data .
+     */
     //error validation
     if (!Array.isArray(array)) {
         console.log("Please provide an array as input.");
@@ -83,4 +95,35 @@ let array = [10, -5, 8, -10, 60, -9, 55, -7, -87, 100];
 const isdataIs = findingBadData(array);
 console.log(isdataIs);
 
+
 //problem-5 
+function gemsToDiamond(firstFriend , secondFriend, thirdFriend){
+    /**
+     * gemsToDiamond is used for show the diamonds after multiplies  three friends gems and compare all diamonds . if totaldiamonds is grater then 2000 then is substract 2000 from totaldiamond else return totaldiamond.  
+     */
+
+    //error validation
+        if (typeof firstFriend !== 'number' || isNaN(firstFriend) || typeof secondFriend !== 'number' || isNaN(secondFriend) || typeof thirdFriend !== 'number' || isNaN(thirdFriend)) {
+            return 'Input must be a number';
+          }
+        let firstFrndGems = 21;
+        let secondFrndGems = 32;
+        let thirdFrndGems = 43;
+    
+        let totalDiamond = (firstFriend * firstFrndGems) + (secondFriend * secondFrndGems) + (thirdFriend * thirdFrndGems);
+    
+        if( totalDiamond >= 2000){
+            return totalDiamond - 2000 ;
+            
+        }
+        else {
+            return totalDiamond ;
+        }
+    
+    }
+    
+    let input1 = 1 ;
+    let input2 = 1 ;
+    let input3 = 1 ;
+    let finalDiamondIs = gemsToDiamond(input1 , input2, input3);
+    console.log(finalDiamondIs);
